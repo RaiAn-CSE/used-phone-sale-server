d0675d6eb1ccddc87d3c05c9962354bd5abb32f9
RaiAn-CSE <robiul-1512392@diu.edu.bd>
2022-11-24 17:03:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const jwt = require('jsonwebtoken');
+require('dotenv').config();
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3exxtfz.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Used Mobile server is running');
+})
+
+app.listen(port, () => console.log(`Used Mobile running on ${port}`))
